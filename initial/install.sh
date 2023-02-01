12118d5857936cbd8fc45bb32b6a66da93b55362
parkdongho <parkdongho28@naver.com>
2023-02-01 23:12:22 +0900

    Add: Vivado Installer

--- initial/install.sh
+++ initial/install.sh
@@ -1,4 +1,4 @@
-sudo yum install net-tools vim make openssh-server zsh firewalld
+sudo yum install net-tools vim make openssh-server zsh firewalld python3 python3-pip
 
 #install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
