4ff9ef9030ab724f7b277b428ecdae4e6865a05e
Dongho Park <parkdongho28@naver.com>
2023-02-15 15:48:27 +0900

    Update install.sh

--- all/install.sh
+++ all/install.sh
@@ -1,9 +1,9 @@
 # yum install
 sudo yum install net-tools vim make openssh-server zsh firewalld gcc
 sudo yum groupinstall "Development Tools" 
+sudo dnf install wget yum-utils make gcc openssl-devel bzip2-devel libffi-devel zlib-devel
 
 # python 3.10 ################################
-sudo dnf install wget yum-utils make gcc openssl-devel bzip2-devel libffi-devel zlib-devel
 wget https://www.python.org/ftp/python/3.10.8/Python-3.10.8.tgz
 tar xzf Python-3.10.8.tgz
 cd Python-3.10.8
@@ -17,10 +17,3 @@ cd ~/python-app/
 sudo /usr/local/bin/python3.10 -m venv appenv
 source appenv/bin/activate 
 ##############################################
-
-
-# nvim
-sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-sudo yum install neovim python3-neovim
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
