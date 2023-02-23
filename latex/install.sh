b8c2c82aaa62126a600d4a1d251647626648c51a
parkdongho <parkdongho28@naver.com>
2023-02-23 16:29:42 +0900

    Update: latex installer

--- latex/install.sh
+++ latex/install.sh
@@ -4,7 +4,7 @@ wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz # or cu
 zcat install-tl-unx.tar.gz | tar xf -
 #cd /tmp/install-tl-20230203/ #에러 발생시 폴더명 변경
 cd /tmp/install-tl-20230209/
-perl ./install-tl --no-interaction # as root or with writable destination
+sudo perl ./install-tl --no-interaction # as root or with writable destination
 
 # Trouble Shooting
 sudo yum install perl-Time-HiRes # Update perl-Time-HiRes
